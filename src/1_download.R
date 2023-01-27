d0d75a42a386c357e6747e9a9bd7e84634adb5c0
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2023-01-27 12:17:55 +0800

    feat: to csv

--- src/1_download.R
+++ src/1_download.R
@@ -3,12 +3,7 @@ library(jsonlite)
 # create /data folder
 dir.create('data', showWarnings = FALSE)
 
-# create default filename based on timestamp
-current_time <- format(Sys.time(), '%Y%m%d')
-fname <- paste('./data/', current_time, '.csv', sep = '')
-
-
-# save commutag to csv file ---------------------------------------
+# read commutag to RData ---------------------------------------
 dataset <- '63528cc34f042e88cc951433'
 all_link <-
   'https://commutag.agawork.tw/dataset/list-image?all=1&dataset='
@@ -16,7 +11,7 @@ limit_link <-
   'https://commutag.agawork.tw/dataset/list-image?dataset='
 
 
-is_limit = TRUE
+is_limit = FALSE
 if (is_limit) {
   str_url <- paste(limit_link, dataset, sep = '')
   json <- jsonlite::fromJSON(str_url)
@@ -26,10 +21,7 @@ if (is_limit) {
   json <- jsonlite::fromJSON(str_url)
   df_img <- json$data
 }
-
-write.csv(df_img, file = fname)
-# save end ----------------------------------------------------------
-
+# end ---------------------------------------------------------------
 
 # save into df_img and lst_info into RData --------------------------
 str_url <-
@@ -38,5 +30,5 @@ str_url <-
         sep = '')
 json <- jsonlite::fromJSON(str_url)
 lst_info <- json$data
-save(df_img, lst_info, file = './data/raw.RData')
+save(df_img, lst_info, file = './data/1.RData')
 # end ---------------------------------------------------------------
--- src/2_cleaning.R
+++ src/2_cleaning.R
@@ -1,4 +1,50 @@
 # load df as global variable
-load('./data/raw.RData')
+load('./data/1.RData')
 
-lst_info$form$itemArr
+# rename formReply to rational column names --------------
+form <- df_img$formReply
+item <- lst_info$form$itemArr # has quest, id
+indices <- match(names(form), item$id)
+names(form) <- item$quest[indices]
+df_img$formReply <- form
+# end ----------------------------------------------------
+
+
+# simplify form reply i.e. remove `value` ----------------
+myUnlist <- function(x) {
+  l <- unlist(x, recursive = FALSE, use.names = FALSE)
+  if (is.list(l)) {
+    x <- sapply(l, function(y)
+      paste(y, collapse = '|')) # 使用|當作checkbox的多選分隔符號
+  } else {
+    x <- l
+  }
+}
+form <- df_img$formReply
+form <- sapply(form, myUnlist)
+
+# remove column version
+df_img$formReply <- NULL
+df_img <- cbind(df_img, form)
+# keep formReply version
+# df_img$formReply <- form
+
+cleaned_img <- df_img
+
+# 先不管verification，如果沒有會出現Error in utils::write.table(cleaned_img, file = fname, fileEncoding = "UTF-8",  :
+# unimplemented type 'list' in 'EncodeElement'，以後再說
+cleaned_img <-
+  apply(df, which(names(df) == 'verification'), as.character)
+
+# end ----------------------------------------------------
+
+
+# create default filename based on timestamp
+current_time <- format(Sys.time(), '%Y%m%d')
+fname <- paste('./data/', current_time, '.csv', sep = '')
+write.csv(cleaned_img,
+          file = fname,
+          fileEncoding = 'UTF-8')
+
+
+save(df_img, file = './data/2.RData')

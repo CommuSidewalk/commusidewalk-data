00e194b28c8e577996872720fe8c874059409d7f
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2023-01-27 16:57:58 +0800

    feat: rename column into wuulong style

--- src/2_cleaning.R
+++ src/2_cleaning.R
@@ -1,75 +1,81 @@
+library(dplyr)
+
 # load df as global variable
 load('./data/1.RData')
 
+df <- df_img
+
 # 謎之quest id xfnxjyq5rrk，或許是編輯表單後遺留下來的，先刪掉
-df_img$formReply$xfnxjyq5rrk <- NULL
+df$formReply$xfnxjyq5rrk <- NULL
+
 
 # rename formReply to rational column names --------------
-form <- df_img$formReply
+form <- df$formReply
 item <- lst_info$form$itemArr # has quest, id
 indices <- match(names(form), item$id)
 names(form) <- item$quest[indices]
-df_img$formReply <- form
+df$formReply <- form
 # end ----------------------------------------------------
 
-
+myPaste <- function(x) {
+  return <- paste(x, collapse = '|') # use | as deliminator
+}
 
 # simplify form reply i.e. remove `value` ----------------
 myUnlist <- function(x) {
   l <- unlist(x, recursive = FALSE, use.names = FALSE)
   if (is.list(l)) {
-    x <- sapply(l, function(y)
-      paste(y, collapse = '|')) # 使用|當作checkbox的多選分隔符號
+    x <- sapply(l, myPaste)
   } else {
     x <- l
   }
 }
-form <- df_img$formReply
+form <- df$formReply
 form <- sapply(form, myUnlist)
 
 # remove column version
-df_img$formReply <- NULL
-df_img <- cbind(df_img, form)
+df$formReply <- NULL
+df <- cbind(df, form)
 # keep formReply version
-# df_img$formReply <- form
+# df$formReply <- form
 # end ----------------------------------------------------
 
 
 # rename other columns -----------------------------------
-df <- df_img
-df$"標註者" <- df$annotation$user
+df$annotation_user <- df$annotation$user
 
 
 annotateClean <- function(x) {
   if (is.list(x)) {
     indices <- x$value == 'true'
-    return <- paste(x[indices, 'name'], collapse = '|')
+    return <- myPaste(x[indices, 'name'])
   } else {
     return <- NA
   }
 }
 
-df$"標註結果" <- sapply(df$annotation$annotation, annotateClean)
+df$annotation_result <-
+  sapply(df$annotation$annotation, annotateClean)
 
 
 lst_user <- sapply(df$verification, function(x) {
   if (length(x) != 0) {
-    return <- paste(x$user, collapse = '|')
+    return <- myPaste(x$user)
   } else {
     return <- NA
   }
 })
 lst_agree <- sapply(df$verification, function(x) {
   if (length(x) != 0) {
-    return <- paste(x$agree, collapse = '|')
+    return <- myPaste(x$agree)
   } else {
     return <- NA
   }
 })
-df <- cbind(df, data.frame("驗證者" = lst_user, "驗證同意值" = lst_agree))
+df <-
+  cbind(df,
+        data.frame(verification_user = lst_user, verification_agree = lst_agree))
 
-df$createdAt <- as.POSIXct(df$createdAt)
-df$updatedAt <- as.POSIXct(df$updatedAt)
 
 # delete useless columns
 df$annotation <- NULL
@@ -78,12 +84,69 @@ df$verification <- NULL
 # end ----------------------------------------------------
 
 
+# invert row order (to asc)
+df <- df[order(nrow(df):1), ]
+
+df <- df %>% rename(
+  '上傳者' = 'uploader',
+  '標註者' = 'annotation_user',
+  '標籤' = 'annotation_result',
+  '驗證者' = 'verification_user',
+  '驗證同意值' = 'verification_agree',
+  '原始總寬度－是否夠寬讓輪椅通行' = '原始總寬度－是否夠寬讓輪椅通行',
+  '實際行走路徑中會碰到的最大動態風險' = '行人被迫實際行走路徑中會碰到的最大動態風險',
+)
+
+empty_cols <- c("imageName",
+                "評分a1",
+                "評分b1",
+                "評分c1",
+                "思源地圖類別-無須填寫",
+                "imageUrl")
+
+df[, empty_cols] <- NA
+
+col_order <-
+  c(
+    "imageName",
+    "lat",
+    "lng",
+    "dataTime",
+    "remark",
+    "上傳者",
+    "標註者",
+    "驗證者",
+    "標籤",
+    "createdAt",
+    "updatedAt",
+    "評分a1",
+    "評分b1",
+    "評分c1",
+    "原本有無設置人行空間",
+    "保護性",
+    "原始總寬度－是否夠寬讓輪椅通行",
+    "佔用情形(多選)-導致兩人交會需停讓或淨寬<1.5米",
+    "實際行走路徑中會碰到的最大動態風險",
+    "承受上題風險的頻率",
+    "使用目的",
+    "思源地圖類別-無須填寫",
+    "思源地圖名稱-無須填寫",
+    "imageUrl"
+  )
+df <- df[, col_order]
+
+
+
 # create default filename based on timestamp
 current_time <- format(Sys.time(), '%Y%m%d')
 fname <- paste('./data/', current_time, '.csv', sep = '')
-write.csv(df,
-          file = fname,
-          fileEncoding = 'UTF-8')
+write.csv(
+  df,
+  file = fname,
+  fileEncoding = 'UTF-8',
+  na = '',
+  row.names = FALSE
+)
 
 
 save(df, file = './data/2.RData')

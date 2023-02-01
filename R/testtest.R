86b412d1b5b6eb5a6ac40cec6e74709f1c137e8a
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2023-02-01 22:31:52 +0800

    feat: add village classify

--- R/testtest.R
+++ R/testtest.R
@@ -1,17 +1,41 @@
-# Load purrr library
-library(purrr)
+library('sf')
+library('dplyr')
 
-# Example data frame
-df <- data.frame(a = c("apple", "banana", "mango"), b = c("chocolate", "spaghetti", "pizza"))
+load('data/3.RData')
+df <- df3
 
-# Example lookup table
-l <- list(
-  a = list("apple" = 1, "banana" = 2, "mango" = 3),
-  b = list("chocolate" = 5, "spaghetti" = 3, "pizza" = 9)
-)
+village_nc <- st_read('data/VILLAGE_MOI_1111213.shp')
+commu_nc <-
+  st_read(
+    'output/20230131_rank.csv',
+    options = c("X_POSSIBLE_NAMES=lng", "Y_POSSIBLE_NAMES=lat")
+  )
 
+# set commu_nc coordinate reference system to village_nc crs
+st_crs(commu_nc) <- st_crs(village_nc)
 
-for (col in names(l)) {
-  df[[col]] <- sapply(df[[col]], \(x) l[[col]][[x]])
+vill_df <- st_drop_geometry(village_nc)
+vill_df <-
+  vill_df |> rename(
+    'countyName' = 'COUNTYNAME',
+    'townName' = 'TOWNNAME',
+    'villName' = 'VILLNAME',
+    'villCode' = 'VILLCODE'
+  )
+vill_df$fullName <-
+  paste(vill_df$countyName,
+        vill_df$townName,
+        vill_df$villName,
+        sep = '_')
+
+df[c('countyName', 'townName', 'villName')] <- NA
+
+w <- st_within(commu_nc, village_nc)
+for (i in seq_along(w)) {
+  if (!identical(w[[i]], integer(0))) {
+    df[i, c('villCode', 'countyName', 'townName', 'villName')] <-
+      vill_df[w[[i]], c('villCode', 'countyName', 'townName', 'villName')]
+  }
 }
-       
+
+print('4_village_classification done :)')

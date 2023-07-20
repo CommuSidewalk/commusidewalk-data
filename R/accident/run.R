0a5265a627c4c58b9e47536478d0f20733768206
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2023-07-20 15:10:27 +0800

    feat: add update script

--- R/accident/run.R
+++ R/accident/run.R
@@ -1,7 +1,10 @@
 # 更新資料的邏輯尚未完成
 # 尚未移除「資料庫已有的資料」
-# source("R/accident/clean_and_write_to_csv.R")
-# source("R/accident/write_to_db.R")
-#
-# clean_and_write_to_csv(auto_padding = TRUE)
-# write_to_db()
+source("R/accident/download_NPA_TM_data.R")
+source("R/accident/clean_and_write_to_csv.R")
+source("R/accident/write_to_db.R")
+
+download_NPA_TM_data()
+delete_table()
+clean_and_write_to_csv()
+write_to_db()
--- R/accident/write_to_db.R
+++ R/accident/write_to_db.R
@@ -9,19 +9,29 @@ SQL_PATH <- file.path("R/accident/sql")
 OUTPUT_PATH <- file.path('output', 'accident')
 
 # util function that drop accidents table
-drop_table <- function (sure = FALSE) {
-  if (sure & dbExistsTable(conn, "general_events") & dbExistsTable(conn, "detail_events")) {
+drop_table <- function () {
+  if (dbExistsTable(conn, "general_events") & dbExistsTable(conn, "detail_events")) {
     dbSendQuery(conn, "DROP TABLE detail_events CASCADE;")
     dbSendQuery(conn, "DROP TABLE general_events CASCADE;")
   }
 }
-drop_table(T);
+
+delete_table <- function () {
+  if (dbExistsTable(conn, "general_events") & dbExistsTable(conn, "detail_events")) {
+    print("delete general_events table and detail_events table...")
+    dbSendQuery(conn, "DELETE FROM detail_events;")
+    dbSendQuery(conn, "DELETE FROM general_events;")
+    print("done!")
+  }
+}
 
 write_table <- function (name, DT = NULL) {
+  print("writing new data into table...")
   if (is.null(DT)) {
     DT <- fread(file.path(OUTPUT_PATH, paste0(name, '.csv')))
   }
   dbAppendTable(conn, name, DT)
+  print("done!")
 }
 
 read_SQL <- function (fname) {
